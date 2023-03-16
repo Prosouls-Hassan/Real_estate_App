74dc2643d7e1ef56eeee941d1232a68addd2661e
unknown <hassankhand10@gmail.com>
2023-03-16 09:29:28 -0700

    Final

---
+++ app/services/firebaseConfig.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  enabled: false,
+  config: {
+    apiKey: "g65875gbnb7g57bu-76b876h856rh",
+    authDomain: "yourapp.firebaseapp.com",
+    projectId: "yourapp",
+    storageBucket: "yourapp.appspot.com",
+    messagingSenderId: "876876865767",
+    appId: "1:876876865767:web:7h766gh787h7hh9i",
+    measurementId: "G-GBI76GHNYUN",
+  },
+};
